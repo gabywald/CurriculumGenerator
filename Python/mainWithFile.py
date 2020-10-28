386c6b52307d99fd984f1af6adb55aa91e256077
Gaby Wald <30340425+gabywald@users.noreply.github.com>
2020-10-28 18:43:52 +0100

    Starting ideas for "CV Generation from file"

---
+++ Python/mainWithFile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*- default is unicode in python3 ?
+
+__author__ = "Gabriel Chandesris"
+__copyright__ = "CC Gabriel Chandesris (2020)"
+__credits__ = ""
+__licence__ = "GNU GENERAL PUBLIC LICENSE v3"
+__version__ = "0.1.0"
+__maintainer__ = "Gabriel Chandesris"
+__email__ = "gabywald[at]laposte.net"
+__contact__ = "gabywald[at]laposte.net"
+__status__ = "Development"
+
+## ## ## ## ## Main script to generate several Curriculum according to file passed in parameter
+
+# TODO format and reading of the file !
+
